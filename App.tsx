f9352d695584ecad466376450db4f7f7c7b9bc01
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2019-09-13 16:31:07 -0300

    Trying to get BackgroundFetch working

--- App.tsx
+++ App.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import ControleEstacionamento from './ControleEstacionamento';
+import * as TaskManager from 'expo-task-manager';
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as Expo from 'expo';
+
+export const taskName = 'test';
+
+TaskManager.defineTask(taskName, async() => {
+    console.log('logging scheduled task');
+    const notification = {
+        title: 'running background fetch',
+        body: 'this is a background notification'
+    };
+    Expo.Notifications.presentLocalNotificationAsync(notification);
+    return BackgroundFetch.Result.NewData;
+});
+console.log('task defined');
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <ControleEstacionamento></ControleEstacionamento>
-    </View>
-  );
+    const registerTask = async() => {
+        await BackgroundFetch.registerTaskAsync(taskName);
+        BackgroundFetch.setMinimumIntervalAsync(60);
+        console.log(await TaskManager.getRegisteredTasksAsync());
+    }
+    registerTask();
+    return (
+        <View style={styles.container}>
+            <ControleEstacionamento></ControleEstacionamento>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 100,
-    marginBottom: 100
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 100
+    }
 });
--- ControleEstacionamento.tsx
+++ ControleEstacionamento.tsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { View, TextInput, Text, Button, AsyncStorage, AsyncStorageStatic } from "react-native";
+import React from 'react';
+import { View, TextInput, Text, Button, AsyncStorage, AsyncStorageStatic } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as TaskManager from 'expo-task-manager';
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as Expo from 'expo';
+import { LocalNotification } from 'expo/build/Notifications/Notifications.types';
+import { taskName } from './App';
 
 interface ControleEstacionamentoState {
-    saldo: number,
-    saldoTemp: string,
-    valor: number,
-    valorTemp: string
+    saldo: number;
+    saldoTemp: string;
+    valor: number;
+    valorTemp: string;
+    permNotifications: boolean;
 }
 
 export enum PersistentStorage {
-    'saldo' = 'saldo',
-    'valor' = 'valor'
+    saldo = 'saldo',
+    valor = 'valor'
 }
 
 const setContainerStyle = {
@@ -18,33 +25,34 @@ const setContainerStyle = {
     flexDirection: 'row',
     height: 50,
     width: 200
-}
+};
 
 const inputStyle = {
     borderColor: '#555',
     borderWidth: 1,
     borderStyle: 'solid',
     borderRadius: 3
-}
+};
 
 const saldoStyle = {
     fontSize: 70
-}
+};
 
 const inputContainerStyle = {
     width: 200,
     height: 40,
     marginRight: 10
-}
+};
 
 const buttonContainerStyle = {
     width: 80,
     height: 40
-}
+};
 
 export default class ControleEstacionamento extends React.Component {
     state: ControleEstacionamentoState;
     asyncStorage: AsyncStorageStatic;
+    expoPermissions: any;
 
     constructor(props) {
         super(props);
@@ -53,88 +61,115 @@ export default class ControleEstacionamento extends React.Component {
             saldo: 90,
             saldoTemp: `90`,
             valor: 4.5,
-            valorTemp: `4.5`
+            valorTemp: `4.5`,
+            permNotifications: false
         };
         this.asyncStorage.getItem(PersistentStorage.saldo, this.saveSaldo.bind(this));
         this.asyncStorage.getItem(PersistentStorage.valor, this.saveValor.bind(this));
+        this.askNotifPermissions();
     }
 
+    askNotifPermissions = async () => {
+        this.expoPermissions = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+        if (this.expoPermissions.status !== 'granted') {
+            Permissions.askAsync(Permissions.NOTIFICATIONS);
+        }
+    };
+
+    sendNotification = async (title: string, body: string) => {
+        const notification: LocalNotification = {
+            title: title,
+            body: body
+        };
+        Expo.Notifications.presentLocalNotificationAsync(notification);
+    };
+
     render() {
-        return (<View>
-            <Text>Valor do estacionamento:</Text>
-            <View style={setContainerStyle}>
-                <View style={inputContainerStyle}>
-                    <TextInput style={inputStyle} value={this.state.valorTemp.toString()} onChangeText={this.setValorTemp.bind(this)}></TextInput>
+        return (
+            <View>
+                <Text>Valor do estacionamento:</Text>
+                <View style={setContainerStyle}>
+                    <View style={inputContainerStyle}>
+                        <TextInput
+                            style={inputStyle}
+                            value={this.state.valorTemp.toString()}
+                            onChangeText={this.setValorTemp.bind(this)}
+                        ></TextInput>
+                    </View>
+                    <View style={buttonContainerStyle}>
+                        <Button title='Set' onPress={this.setValor.bind(this)}></Button>
+                    </View>
                 </View>
-                <View style={buttonContainerStyle}>
-                    <Button title="Set" onPress={this.setValor.bind(this)}></Button>
+                <Text>Forçar saldo atual no cartão:</Text>
+                <View style={setContainerStyle}>
+                    <View style={inputContainerStyle}>
+                        <TextInput
+                            style={inputStyle}
+                            value={this.state.saldoTemp.toString()}
+                            onChangeText={this.setSaldoTemp.bind(this)}
+                        ></TextInput>
+                    </View>
+                    <View style={buttonContainerStyle}>
+                        <Button title='Set' onPress={this.setSaldo.bind(this)}></Button>
+                    </View>
                 </View>
+                <Text>Valor da diaria:</Text>
+                <Text>{this.state.valor}</Text>
+                <Text>Saldo atual:</Text>
+                <Text style={saldoStyle}>{this.state.saldo}</Text>
+                <Button title='-' onPress={this.decreaseSaldo.bind(this)}></Button>
             </View>
-            <Text>Forçar saldo atual no cartão:</Text>
-            <View style={setContainerStyle}>
-                <View style={inputContainerStyle}>
-                    <TextInput style={inputStyle} value={this.state.saldoTemp.toString()} onChangeText={this.setSaldoTemp.bind(this)}></TextInput>
-                </View>
-                <View style={buttonContainerStyle}>
-                    <Button title="Set" onPress={this.setSaldo.bind(this)}></Button>
-                </View>
-            </View>
-            <Text>Valor da diaria:</Text>
-            <Text>{this.state.valor}</Text>
-            <Text>Saldo atual:</Text>
-            <Text style={saldoStyle}>{this.state.saldo}</Text>
-            <Button title="-" onPress={this.decreaseSaldo.bind(this)}></Button>
-        </View>);
+        );
     }
 
     setSaldo() {
         this.asyncStorage.setItem(PersistentStorage.saldo, this.state.saldoTemp, this.errorFunc);
-        this.setState(previousState => {
-            return ({
+        this.sendNotification('Controle Estacionamento', 'Saldo salvo.');
+        this.setState((previousState) => {
+            return {
                 ...previousState,
                 saldo: Number(this.state.saldoTemp)
-            })
-        })
+            };
+        });
     }
 
     setValor() {
         this.asyncStorage.setItem(PersistentStorage.valor, this.state.valorTemp, this.errorFunc);
-        this.setState(previousState => {
-            return ({
+        this.sendNotification('Controle Estacionamento', 'Valor salvo.');
+        this.setState((previousState) => {
+            return {
                 ...previousState,
                 valor: Number(this.state.valorTemp)
-            })
-        })
+            };
+        });
     }
 
     setSaldoTemp(text: string) {
-        this.setState(previousState => {
-            return ({
+        this.setState((previousState) => {
+            return {
                 ...previousState,
                 saldoTemp: text
-            })
-
-        })
+            };
+        });
     }
 
     setValorTemp(text: string) {
-        this.setState(previousState => {
-            return ({
+        this.setState((previousState) => {
+            return {
                 ...previousState,
                 valorTemp: text
-            })
-
-        })
+            };
+        });
     }
 
     decreaseSaldo() {
         if (this.state.saldo >= this.state.valor) {
-            this.setState(previousState => {
-                return ({
+            this.setState((previousState) => {
+                return {
                     ...previousState,
-                    saldo: (this.state.saldo - this.state.valor)
-                })
-            })
+                    saldo: this.state.saldo - this.state.valor
+                };
+            });
         }
         this.asyncStorage.setItem(PersistentStorage.saldo, this.state.saldo.toString(), this.errorFunc);
     }
@@ -147,28 +182,26 @@ export default class ControleEstacionamento extends React.Component {
 
     saveValor(error, result) {
         if (!error && result) {
-            this.setState(previousState => {
-                return ({
+            this.setState((previousState) => {
+                return {
                     ...previousState,
                     valor: Number(result)
-                })
-            })
-        }
-        else if (result === null) {
+                };
+            });
+        } else if (result === null) {
             this.asyncStorage.setItem(PersistentStorage.valor, this.state.valor.toString(), this.errorFunc);
         }
     }
 
     saveSaldo(error, result) {
         if (!error && result) {
-            this.setState(previousState => {
-                return ({
+            this.setState((previousState) => {
+                return {
                     ...previousState,
                     saldo: Number(result)
-                })
-            })
-        }
-        else if (result === null) {
+                };
+            });
+        } else if (result === null) {
             this.asyncStorage.setItem(PersistentStorage.saldo, this.state.saldo.toString(), this.errorFunc);
         }
     }
